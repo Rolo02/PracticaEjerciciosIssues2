cffb892a954a7a614a66948a0dcafe4504b19db5
Raul <raul.sanand.3@educa.jcyl.es>
2025-11-27 08:21:44 +0100

    Añadimos el codigo a RegistroUsuario2.java

---
+++ RegistroUsuario2.java
@@ -0,0 +1,13 @@
+El proyecto tiene la clase RegistroUsuario.java:
+public class RegistroUsuario {
+
+    public boolean registrarUsuario(String nombre, String email) {
+        //  ERROR: no se valida el email
+        if (nombre == null || nombre.isEmpty() || email == null || email.isEmpty()) {
+            return false;
+        }
+        // Aquí se simula que el usuario queda registrado
+        System.out.println("Usuario registrado: " + nombre + " con email " + email);
+        return true;
+    }
+}
